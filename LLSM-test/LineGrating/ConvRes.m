997c1060d8510a624be0c88a8ba19eb3c2629fa8
andyzjc <jz0036@princeton.edu>
2023-05-13 12:04:28 -0400

    correct noise for line grating

--- LLSM-test/LineGrating/ConvRes.m
+++ LLSM-test/LineGrating/ConvRes.m
@@ -5,9 +5,9 @@ function [convLines,lineSpot,Spacing,LineZ] = ConvRes(PSFexc,PSFdet,SNR)
     % lineSpot = [0.0000,0.2202,0.4770,0.7705,1.1008,1.4677,1.8713,2.3116,...
     %             2.7886,3.3023,3.8526,4.4397,5.0635,5.7239,6.4211,7.1549,...
     %             7.9254,8.7326,9.5765,10.4571,14.0896]; %um 
-    N_line = 2049;
-    deltax_line = 0.01; %um
-    Spacing = 0.22:0.03:0.9; % um
+    N_line = 1025;
+    deltax_line = 0.02; %um
+    Spacing = 0.22:0.02:0.9; % um
     lineSpot = zeros(1,length(Spacing)+1);
     lineSpot(1,1) = 1;
     for i = 1:length(Spacing)
@@ -23,11 +23,11 @@ function [convLines,lineSpot,Spacing,LineZ] = ConvRes(PSFexc,PSFdet,SNR)
     % get overall PSF
     PSFoverall = PSFexc .* PSFdet;
     xzPSFOveralldecon = PSFoverall(:,:,(N+1)/2);
-    % xzPSFOverall = xzPSFOveralldecon + poissrnd(xzPSFOveralldecon) .* 1/SNR;
+    %xzPSFOveralldecon = xzPSFOveralldecon + poissrnd(xzPSFOveralldecon) .* 1/SNR;
     
     % interpolate overall PSF
    scaledxzPSFOverall = imresize(xzPSFOveralldecon,[N_line,N_line]);
-   scaledxzPSFOverall = scaledxzPSFOverall + poissrnd(scaledxzPSFOverall) .* 1/SNR;
+   %scaledxzPSFOverall = scaledxzPSFOverall + poissrnd(scaledxzPSFOverall) .* 1/SNR;
 
     % Convolution with GT lines
     convLines = conv2(GTLines,scaledxzPSFOverall','same');
